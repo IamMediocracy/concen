1c3211344688f71bcc26e64a6d23b817e6d350ce
Zachary Heins <zack+bit@heinstechnology.com>
2018-11-19 08:06:36 -0500

    Exercise 5
    Building an expandable table to show a summary of the character past a few cursory details in the row

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,100 @@ import { buildBreadcrumb, buildNavigationItems } from './common/Components.jsx';
 // Define component class
 export default class Application extends Component {
   // Initial Constructor
-  constructor(props) {
-    super(props) && autoBind(this);
-    this.state = {
+  constructor( props ) {
+    super( props ) && autoBind( this );
+    this.render = this.render.bind( this );
+    this.state  = {
       page: 'page1',
       increment: 10,
-      text: exampleText
+      text: exampleText,
+      characters: [],
+      planets: [],
+      vehicles: [],
+      films: [],
+      species: []
     };
   }
-  // Update state
-  updateState(state) {
-    return this.setState(state);
+  
+  componentDidMount() {
+    this.getPeople( 'https://swapi.co/api/people/' );
+    this.getPlanets( 'https://swapi.co/api/planets/' );
+    this.getVehicles( 'https://swapi.co/api/vehicles/' );
+    this.getFilms( 'https://swapi.co/api/films/' );
+    this.getSpecies( 'https://swapi.co/api/people/' );
   }
+  
+  getPeople( url ) {
+    fetch( url )
+      .then( results => {
+        return results.json();
+      } ).then( data => {
+        this.setState( { characters: this.state.characters.concat( data.results ) } );
+        if ( data.next != null ) {
+          this.getPeople( data.next );
+        }
+      }
+    )
+  }
+  
+  getPlanets( url ) {
+    fetch( url )
+      .then( results => {
+        return results.json();
+      } ).then( data => {
+        this.setState( { planets: this.state.planets.concat( data.results ) } );
+        if ( data.next != null ) {
+          this.getPlanets( data.next );
+        }
+      }
+    )
+  }
+  
+  getVehicles( url ) {
+    fetch( url )
+      .then( results => {
+        return results.json();
+      } ).then( data => {
+        this.setState( { vehicles: this.state.vehicles.concat( data.results ) } );
+        if ( data.next != null ) {
+          this.getVehicles( data.next );
+        }
+      }
+    )
+  }
+  
+  getFilms( url ) {
+    fetch( url )
+      .then( results => {
+        return results.json();
+      } ).then( data => {
+        this.setState( { films: this.state.films.concat( data.results ) } );
+        if ( data.next != null ) {
+          this.getFilms( data.next );
+        }
+      }
+    )
+  }
+  
+  getSpecies( url ) {
+    fetch( url )
+      .then( results => {
+        return results.json();
+      } ).then( data => {
+        this.setState( { species: this.state.species.concat( data.results ) } );
+        if ( data.next != null ) {
+          this.getSpecies( data.next );
+        }
+        console.log( data.results );
+      }
+    )
+  }
+  
+  // Update state
+  updateState( state ) {
+    return this.setState( state );
+  }
+  
   // Render
   render() {
     return (
@@ -36,15 +118,15 @@ export default class Application extends Component {
           <Menu
             theme="dark"
             mode="horizontal"
-            selectedKeys={[this.state.page]}
+            selectedKeys={[ this.state.page ]}
             style={{ lineHeight: '64px' }}
-            onClick={item => handleOnPageNavigation.call(this,item)}>
-            {buildNavigationItems(['page1', 'page2'])}
+            onClick={item => handleOnPageNavigation.call( this, item )}>
+            {buildNavigationItems( [ 'page1', 'page2' ] )}
           </Menu>
         </Layout.Header>
         <Layout.Content style={{ padding: '0 50px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
-            {buildBreadcrumb(['Home',this.state.page])}
+            {buildBreadcrumb( [ 'Home', this.state.page ] )}
           </Breadcrumb>
           <div
             style={{
@@ -52,7 +134,7 @@ export default class Application extends Component {
               padding: 24,
               minHeight: 'calc(100vh - 186px)'
             }}>
-            {pageRouter(this.state.page)(this.state, this.updateState)}
+            {pageRouter( this.state.page )( this.state, this.updateState )}
           </div>
         </Layout.Content>
         <Layout.Footer style={{ textAlign: 'center' }}>
--- src/PageTwo.jsx
+++ src/PageTwo.jsx
@@ -2,10 +2,73 @@
 
 // Load NPM modules
 import React from 'react';
+import { Table } from 'antd';
+
+const getRow = ( character, state ) => {
+  
+  let films     = character.films.forEach( e => state.films.find( f => {
+    if ( f.url == e ) return f.name;
+  } ) );
+  let homeworld = state.planets.find( p => {
+    if ( p.url == character.homeworld ) return p.name;
+  } );
+  let vehicles  = character.vehicles.forEach( v => state.vehicles.find( f => {
+    if ( f.url == v ) return f.name;
+  } ) );
+  
+  character[ "description" ] = `${character.name} is from the planet ${homeworld}, has appeared in the films ${films}, and can operate the following vehicles: ${vehicles}`;
+  return character;
+}
+
+const getColumns = () => {
+  return [ {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'Name'
+  },
+    {
+      title: 'Birth Year',
+      dataIndex: 'birth_year',
+      key: 'Birth Year'
+    },
+    {
+      title: 'Eye Color',
+      dataIndex: 'eye_color',
+      key: 'Eye Color'
+    }, {
+      title: 'Gender',
+      dataIndex: 'gender',
+      key: 'Gender'
+    }, {
+      title: 'Height',
+      dataIndex: 'height',
+      key: 'Height'
+    }, {
+      title: 'Weight',
+      dataIndex: 'mass',
+      key: 'Weight'
+    } ]
+}
+
+const getDataSource = state => {
+  let characters = [];
+  state.characters.forEach( e => characters.push( getRow( e, state ) ) );
+  return characters;
+}
+
+// Build table
+const buildTable = ( dataSource, columns ) => (
+  <Table dataSource={dataSource} columns={columns}
+         expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>}/>
+);
 
 // Define export
-export default (state, updateState) => (
+export default ( state, updateState ) => (
   <div className="page-two">
-    <p>Page Two</p>
+    <h2>Click a character to expand for more info:</h2>
+    {buildTable(
+      getDataSource( state ),
+      getColumns()
+    )}
   </div>
 );
